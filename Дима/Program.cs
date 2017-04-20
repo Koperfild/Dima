a23d243e15ccdc1806567a556ee24bfcd307c878
Koperfild <Avalancher2@rambler.ru>
2017-04-20 20:23:25 +0300

    Add project files.

---
+++ Дима/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Занятия
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Тут делаем сумму чисел. c задаём с клавиатуры
+            //int c;
+            //c = 22;
+            //c = c + 5;
+            //Console.WriteLine(c);
+            //int a = 10;
+            //int b = 20;
+            Print();
+            Console.ReadKey();
+            && - И
+               x== 5 || x==10
+            if (x>=50 && x<=100 && y>50 && y < 60)
+            {
+
+            }
+                //double b = 2.1;
+                //double k = 2;
+                //Console.WriteLine("Введите х");
+                //double x = double.Parse(Console.ReadLine());
+
+                //Console.WriteLine(k +"*" + x + " + " + b + " = " + k * x + b);
+                //Console.ReadKey();
+                //int result = a + b;
+
+                //Console.WriteLine("Сложение " + a + " + " + b + " = " + result);
+                //Console.ReadKey();
+                //Console.WriteLine(a);
+                //Console.WriteLine(result);
+
+
+
+                //Написать код. Завести переменную sum типа double. Завести 5 переменных double. Сложить эти 5 переменных и сохранить их сумму в переменной sum.
+                //Посчитать среднее этих 5 чисел (sum/кол-во, т.е. sum / 5.0)
+                //Если Среднее больше первого числа, то вывести на экран строку "первое число меньше среднего"
+                //Если Среднее меньше первого числа, то вывести на экран строку "первое число больше среднего"
+                //Сумма оказывается больш
+
+        }
+
+
+
+        static void Print()
+        {
+            string str1 = "Hello ";
+            string str2 = "World!";
+            string res = str1 + str2;
+            Console.WriteLine(res);
+        }
+
+    }
+}
---
+++ Занятие2/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Занятие2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double a = 1;
+            //double b = 6;
+            //double c = 10;
+            //double d = 15;
+            //double i = 1.5;
+
+            //double sum = a + b + c + d + i;
+            //Console.WriteLine("Сумма чисел равна:");
+            //Console.WriteLine(sum + "\n\n");
+
+            //double srednee = sum / 5;
+
+            //Console.WriteLine("Первое число: " + a + " " + "Среднее число: " + srednee);
+
+
+            //if (a > srednee)
+            //    Console.WriteLine("Первое число больше среднего");
+            //else if (a < srednee)
+            //    Console.WriteLine("Первое число меньше среднего");
+            //else if (a == srednee)
+            //    Console.WriteLine("Первое число равно среднему");
+            //else
+            //{
+
+            //}
+
+
+            //for (1; 2; 3)
+            //{
+
+            //}
+
+            //int a;
+            //a++;
+            //a--
+            //++a;
+            //a = a + 1;
+            //a += 5;
+            //a = a + 5;
+            //a /= 10;
+            //a -= 10;
+            //a += 10;
+
+
+
+            //for (int i = 10; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    //if ((i % 2) == 1)
+            //    //{
+            //    //    continue;
+            //    //}
+            //    if (i == 6)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(" i равно " + i + "\n\n");
+
+            //    for (int j = i - 1; j > 0; j--)
+            //    {
+            //        Console.WriteLine("\t" + j);
+            //    }
+
+
+            //    //if ((i % 2) == 0)
+            //    //{
+            //    //    Console.WriteLine(i);
+            //    //}
+            //}
+            ////int k = 11;
+            ////double peremennaya = Percents(k);
+
+            ////Console.WriteLine(peremennaya);
+
+            string stroka = "Summer time";
+            int newlines = 10;
+            int chisloProbelov = 5;
+
+            Pechat(stroka, newlines, chisloProbelov);
+
+            Pechat(stroka, newlines, 0);
+            Pechat(stroka, 0, 0);
+            Pechat(stroka);
+
+            VesVodi(20);
+            VesVodi(20, 200, 3, 144);
+
+            Pechat(stroka, newlines);
+            Pechat(stroka);
+
+            Console.ReadKey();
+
+            Console.Clear();
+
+            Pechat("Другая строка", 20, 20);
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        private static double Percents(int a, int b, int c, double d, double e)
+        {
+            double result = a * 0.01;
+            Console.WriteLine("Мы внутри метода");
+            return result;
+        }
+        private static void Pechat(string s, int lines, int probeli)
+        {
+            for (int i = 0; i < lines; i++)
+            {
+                Console.Write("\n");
+            }
+            for (int i = 0; i < probeli; i++)
+                Console.Write(" ");
+
+            Console.WriteLine(s);
+        }
+        private static void Pechat(string s, int lines)
+        {
+            Pechat(s, lines, 0);
+        }
+        private static void Pechat(string s)
+        {
+            Pechat(s, 0, 0);
+        }
+        private static double VesVodi(double V, double temperatura, double yrovenSoli, double visotaNadYrovnemMorya)
+        {
+            Pechat("lalala");
+            return V*temperatura*0.01*yrovenSoli/visotaNadYrovnemMorya * 1.0034;
+        }
+        private static double VesVodi(double V)
+        {
+            return 1;
+        }
+
+    }
+    public class MyClass
+    {
+
+    }
+}
